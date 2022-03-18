67f4e159abbd692592a49c5c286fefddbcd9b8b3
taba1uga <taba1uga@desktop>
2022-03-18 19:12:20 +0100

    Add operators

--- libraries/limits/width.c
+++ libraries/limits/width.c
@@ -1,24 +1,17 @@
 #include <limits.h>
-#include <math.h>
-#include <stdbool.h>
 #include <stdio.h>
-
-/* Width principles: */
-/* 	1. Each data type width is multiple of `unsigned char` width */
-/* 	2. Smaller type cannot be wider than bigger one */
-
-int get_witdh(unsigned long long int max) { return log2l(max); }
 
 void show_width(char *type_name, int width) {
   printf("`%s` is %d bits wide\n\n", type_name, width);
 }
 
 int main(void) {
-  show_width("char", get_witdh(UCHAR_MAX) + 1);
-  show_width("short int", get_witdh(USHRT_MAX) + 1);
-  show_width("int", get_witdh(UINT_MAX) + 1);
-  show_width("long int", get_witdh(ULONG_MAX));
-  show_width("long long int", get_witdh(ULLONG_MAX));
+  /* Each data type width is multiplication of `unsigned char` width */
+  show_width("char", sizeof(char)*CHAR_BIT);
+  show_width("short int",sizeof(short int)*CHAR_BIT);
+  show_width("int", sizeof(int)*CHAR_BIT);
+  show_width("long int", sizeof(long int)*CHAR_BIT);
+  show_width("long long int", sizeof(long long int)*CHAR_BIT);
 
   return 0;
 }
