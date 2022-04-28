0e696f1793e660abbb3950100f0557183f42676f
kubataba1uga <jakub@buczynski.net.pl>
2022-04-28 13:18:20 +0200

    add uchar conertion examples

--- types/string/char32_to_char.c
+++ types/string/char32_to_char.c
@@ -4,27 +4,27 @@
 #include <uchar.h>
 #include <wchar.h>
 
-#if __STDC_UTF_16__ != 1
-#error "__STDC_UTF_16__ not define"
+#if __STDC_UTF_32__ != 1
+#error "__STDC_UTF_32__ not define"
 #endif
 
 int main(void) {
   setlocale(LC_ALL, "");
-  char16_t *input = u"źdźbło";
+  char32_t *input = U"źdźbło";
 
   size_t input_size = sizeof(input);
 
   char output[input_size];
   char *output_p = output;
 
-  char16_t *input_p = input;
+  char32_t *input_p = input;
 
   size_t convertion_result;
 
   mbstate_t state = {0};
 
   for (size_t i = 0; i < input_size; i++) {
-    convertion_result = c16rtomb(output_p, *input_p, &state);
+    convertion_result = c32rtomb(output_p, *input_p, &state);
     if (convertion_result == (size_t)-1) {
       break; // -1 == invalid code unit sequence detected
       return (EXIT_FAILURE);
