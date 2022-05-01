732e6f6bfe64e7e0c09769e42976fde0c143e4a8
kubaTaba1uga <jakub@buczynski.net.pl>
2022-05-01 20:16:13 +0200

    add file writing example

---
+++ libraries/stdio/write_to_file.c
@@ -0,0 +1,37 @@
+/* The full list of file creation flags and file status */
+/*	can be found by command:	  `man open` */
+#include <stdio.h>
+#include <string.h>
+
+#include "utils.h"
+
+#define WORD_SIZE 24
+
+int main(void) {
+  char path_prefix[] = "/tmp/";
+  size_t path_size = (size_t)WORD_SIZE + sizeof(path_prefix);
+  char path[path_size];
+  size_t word_size = (size_t)WORD_SIZE;
+
+  char word[word_size];
+
+  create_random_word(word, word_size);
+
+  sprintf(path, "%s%s", path_prefix, word);
+
+  FILE *my_file = fopen(path, "w+b");
+  if (!my_file) {
+    perror("File opening failed");
+    return EXIT_FAILURE;
+  }
+
+  create_random_word(word, word_size);
+  word[word_size - 2] = '\n';
+  word[word_size - 1] = '\0';
+
+  int count = fwrite(word, sizeof(char), word_size, my_file);
+  if (count != word_size)
+    perror("File writing failed");
+  else
+    printf("File path: %s\n", path);
+}
